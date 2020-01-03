69d9c1d3e1ef198dbfffa127492165ff311d4dcd
Sergio Burgueño <burguer@gmail.com>
2020-01-03 15:46:09 -0800

    Add initial state

--- src/app.rb
+++ src/app.rb
@@ -1,1 +1,8 @@
-puts "Hello world"
+require_relative "view/ruby2d"
+require_relative "model/state"
+
+view = View::Ruby2dView.new
+
+initial_state = Model::initial_state
+
+view.render(initial_state)
--- src/model/state.rb
+++ src/model/state.rb
@@ -16,4 +16,15 @@ module Model
   class State < Struct.new(:snake, :food, :grid)
   end
 
+  def self.initial_state
+    Model::State.new(
+      Model::Snake.new([
+          Model::Coord.new(1,1),
+          Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(4,4),
+      Model::Grid.new(8, 12)
+    )
+  end
+
 end
